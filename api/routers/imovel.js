6158178278f7c70661761090c7817fe18d061984
Pedro <pedrospoljaric@hotmail.com>
2019-03-09 17:53:40 -0300

    structure

--- api/routers/imovel.js
+++ api/routers/imovel.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const ImovelController = require('../controllers/imovel');
 
+router.get('/:id', (req, res, next) => {
+    ImovelController.get(req.params.id).then(dados => {
+        res.json({ success: true, data: dados })
+    }).catch(next)
+})
+
 module.exports = router;
--- api/routers/usuario.js
+++ api/routers/usuario.js
@@ -3,11 +3,20 @@ const router = express.Router();
 
 const UsuarioController = require('../controllers/usuario');
 
+
 router.post('/', (req, res, next) => {
-    UsuarioController.criarUsuario(req.body).then(dados => {
+    UsuarioController.create(req.body).then(dados => {
         res.json({ success: true, data: dados })
     }).catch(next)
 })
+
+router.get('/:id', (req, res, next) => {
+    UsuarioController.get(req.params.id).then(dados => {
+        res.json({ success: true, data: dados })
+    }).catch(next)
+})
+
+// put, delete usam body tambem
 
 router.get('/exemplo', (req, res, next) => {
     UsuarioController.exemplo(req.query).then(dados => {
