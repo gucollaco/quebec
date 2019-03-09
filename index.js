77c911272512abf406760dc6ecfd67df1baaf031
dsalexan <danilo.salexan@gmail.com>
2019-03-09 16:02:46 -0300

    making view

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 const server = require('http').createServer(app);
 require('dotenv').config();
 
 const cors = require('cors');
+
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        title: function () { return 'Quebec'; }
+    }
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 const router = require('./routers/router')
 
 app.use(cors());
+app.use(express.static('public'));
+
+app.use('/', express.static(__dirname + '/node_modules/onsenui'));
+
 app.use('/', cors(), router);
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
